c389b4900c74e11193cf76ba3b65c41316e38b30
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-06-10 02:17:18 -0300

    feat: add initial Clerk authentication

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import RootProviders from "@/components/providers/RootProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Wallet Wizard",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html
+        lang="en"
+        className="dark"
+        style={{
+          colorScheme: "dark",
+        }}
+      >
+        <body className={inter.className}>
+          <RootProviders>
+            {children}
+          </RootProviders>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
