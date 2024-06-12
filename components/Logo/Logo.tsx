c7d6d6aac3e17dcd1da50654cbfc6229caf6beab
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-06-12 00:46:22 -0300

    feat: add Navbar component

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -13,8 +13,8 @@ const LogoDesktop = () => {
     return (
         <div className="hidden md:block">
             <a href="/" className="flex items-center gap-2 ">
-                <Wallet className="stroke h-8 w-8 stroke-violet-400 stroke-[1.5]" />
-                <p className="bg-gradient-to-r from-violet-400 to-orange-400 bg-clip-text text-[27px] font-bold leading-tight tracking-tighter text-transparent">WalletWizard</p>
+                <Wallet className="stroke h-9 w-9 stroke-violet-400 stroke-[1.5]" />
+                <p className="bg-gradient-to-r from-violet-400 to-orange-400 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">WalletWizard</p>
             </a>
         </div>
     );
