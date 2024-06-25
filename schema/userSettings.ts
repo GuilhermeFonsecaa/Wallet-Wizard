4f12b65f7846d6050ac4eeac9bd8168ea69bd139
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-06-25 01:37:37 -0300

    feat: add update user currency schema

---
+++ schema/userSettings.ts
@@ -0,0 +1,12 @@
+import { Currencies } from "@/lib/currency"
+import { z } from "zod"
+
+export const UpdateUserCurrencySchema = z.object({
+    currency: z.custom(value => {
+        const found = Currencies.some((c) => c.value === value);
+        if (!found) {
+            throw new Error(`invalid currency: ${value}`)
+        }
+        return value;
+    })
+})
