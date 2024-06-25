503df79866f42ee38bd7fb3d86833ac49e792352
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-06-25 01:36:55 -0300

    feat: add tan stack query client provider

--- components/providers/RootProvider.tsx
+++ components/providers/RootProvider.tsx
@@ -2,11 +2,19 @@
 
 import { ThemeProvider } from "next-themes";
 import React, { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function RootProviders({ children }: { children: ReactNode }) {
-    return <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-        {children}
-    </ThemeProvider>
+    const [queryClient] = React.useState(() => new QueryClient({}))
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+                {children}
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+        </QueryClientProvider>
+    )
 }
 
 export default RootProviders;
