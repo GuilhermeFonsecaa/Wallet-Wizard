359516e9cd6cf6002c402a9b156852b95506b941
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-06-25 01:38:34 -0300

    feat: add currency combobox component

--- components/CurrencyCombobox/CurrencyCombobox.tsx
+++ components/CurrencyCombobox/CurrencyCombobox.tsx
@@ -23,6 +23,11 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 import { Currencies, Currency } from "@/lib/currency"
+import { useMutation, useQuery } from "@tanstack/react-query"
+import SkeletonWrapper from "../SkeletonWrapper/SkeletonWrapper"
+import { UserSettings } from "@prisma/client"
+import { UpdateUserCurrency } from "@/app/wizard/_actions/userSettings"
+import { toast } from "sonner"
 
 
 export function CurrencyCombobox() {
@@ -31,35 +36,86 @@ export function CurrencyCombobox() {
     const [selectedOption, setSelectedOption] = React.useState<Currency | null>(
         null
     )
+    const userSettings = useQuery<UserSettings>({
+        queryKey: ["userSettings"],
+        queryFn: () => fetch("/api/user-settings").then((res) => res.json())
+    })
+
+    const mutation = useMutation({
+        mutationFn: UpdateUserCurrency,
+        onSuccess: (data: UserSettings) => {
+            toast.success("Moeda principal atualizada com sucesso 🎉", {
+                id: "update-currency"
+            });
+            setSelectedOption(
+                Currencies.find((currency) => currency.value === data.currency) || null
+            );
+        },
+
+        onError: (e) => {
+            console.log(e)
+            toast.error("Ocorreu um erro ao atualizar sua moeda principal, tente novamente!", {
+                id: "update-currency"
+            })
+        }
+    });
+
+    //Não criar uma nova instância dessa função toda vez que o componente renderiza, manter memorizada até que mutation mude(atualização da currency)
+    const selectOption = React.useCallback((currency: Currency | null) => {
+        if (!currency) {
+            toast.error("Por favor, selecione sua moeda principal")
+            return;
+        }
+
+        toast.loading("Atualizando sua moeda principal...", {
+            id: "update-currency"
+        })
+        console.log(currency.value)
+        mutation.mutate(currency.value)
+    }, [mutation])
+
+    React.useEffect(() => {
+        if (!userSettings.data) {
+            return;
+        }
+        const userCurrency = Currencies.find((currency) => currency.value === userSettings.data.currency);
+        if (userCurrency) {
+            setSelectedOption(userCurrency);
+        }
+    }, [userSettings.data])
 
     if (isDesktop) {
         return (
-            <Popover open={open} onOpenChange={setOpen}>
-                <PopoverTrigger asChild>
-                    <Button variant="outline" className="w-full justify-start">
-                        {selectedOption ? <>{selectedOption.label}</> : <>+ Definir moeda</>}
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0" align="start">
-                    <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-                </PopoverContent>
-            </Popover>
+            <SkeletonWrapper isLoading={userSettings.isFetching}>
+                <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                        <Button variant="outline" className="w-full justify-start" disabled={mutation.isPending}>
+                            {selectedOption ? <>{selectedOption.label}</> : <>+ Definir moeda</>}
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0" align="start">
+                        <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
+                    </PopoverContent>
+                </Popover>
+            </SkeletonWrapper>
         )
     }
 
     return (
-        <Drawer open={open} onOpenChange={setOpen}>
-            <DrawerTrigger asChild>
-                <Button variant="outline" className="w-full justify-start">
-                    {selectedOption ? <>{selectedOption.label}</> : <>+ Definir moeda</>}
-                </Button>
-            </DrawerTrigger>
-            <DrawerContent>
-                <div className="mt-4 border-t">
-                    <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-                </div>
-            </DrawerContent>
-        </Drawer>
+        <SkeletonWrapper isLoading={userSettings.isFetching}>
+            <Drawer open={open} onOpenChange={setOpen}>
+                <DrawerTrigger asChild>
+                    <Button variant="outline" className="w-full justify-start" disabled={mutation.isPending}>
+                        {selectedOption ? <>{selectedOption.label}</> : <>+ Definir moeda</>}
+                    </Button>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <div className="mt-4 border-t">
+                        <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
+                    </div>
+                </DrawerContent>
+            </Drawer>
+        </SkeletonWrapper>
     )
 }
 
@@ -79,10 +135,10 @@ function OptionList({
                     {Currencies.map((currency: Currency) => (
                         <CommandItem
                             key={currency.value}
-                            value={currency.value}
+                            value={currency.label}
                             onSelect={(value) => {
                                 setSelectedOption(
-                                    Currencies.find((priority) => priority.value === value) || null
+                                    Currencies.find((priority) => priority.label === value) || null
                                 )
                                 setOpen(false)
                             }}
