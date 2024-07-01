e7546b76f3a01101859c438c2ddc39e0f0034311
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-07-01 17:37:34 -0300

    feat: add category picker component

---
+++ app/(darshboard)/_components/CategoryPicker.tsx
@@ -0,0 +1,128 @@
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { TransactionType } from "@/lib/types";
+import { Category } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import React, { useCallback, useEffect } from "react";
+import CreateCategoryDialog from "./CreateCategoryDialog";
+import { CommandItem } from "cmdk";
+import { cn } from "@/lib/utils";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+interface Props {
+  type: TransactionType;
+  onChange: (value: string) => void;
+}
+
+function CategoryRow({ category }: { category: Category }) {
+  return (
+    <div className="flex items-center gap-2 ml-1.5">
+      <span role="img">{category.icon}</span>
+      <span>{category.name}</span>
+    </div>
+  );
+}
+
+const CategoryPicker = ({ type, onChange }: Props) => {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  useEffect(() => {
+    if (!value) {
+      return;
+    }
+    //quando o valor mudar, chame o retorno de chamada onchange
+    onChange(value);
+  }, [onChange, value]);
+
+  const categoriesQuery = useQuery({
+    queryKey: ["categories", type],
+    queryFn: () =>
+      fetch(`/api/categories?type=${type}`).then((res) => res.json()),
+  });
+
+  const selectedCategory = categoriesQuery.data?.find(
+    (category: Category) => category.name === value
+  );
+
+  // evita recriações desnecessárias da função a cada renderização do componente, melhorando a performance
+  const successCallback = useCallback(
+    (category: Category) => {
+      setValue(category.name);
+      setOpen((prev) => !prev);
+    },
+    [setValue, setOpen]
+  );
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {selectedCategory ? (
+            <CategoryRow category={selectedCategory} />
+          ) : (
+            "Selecione a categoria"
+          )}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shirink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <CommandInput placeholder="Pesquisar categoria..." />
+          <CommandEmpty>
+            <p>Categoria não encontrada</p>
+            <p className="text-xs text-muted-foreground">
+              Dica: Crie uma nova categoria
+            </p>
+          </CommandEmpty>
+          <CommandGroup>
+            <CommandList>
+              {categoriesQuery.data &&
+                categoriesQuery.data.map((category: Category) => (
+                  <CommandItem
+                    className="flex items-center gap-2"
+                    key={category.name}
+                    onSelect={() => {
+                      setValue(category.name);
+                      setOpen((prev) => !prev);
+                    }}
+                  >
+                    <CategoryRow category={category} />
+                    <Check
+                      className={cn(
+                        "mt-1 w-4 h-4 opacity-0",
+                        value === category.name && "opacity-100"
+                      )}
+                    ></Check>
+                  </CommandItem>
+                ))}
+            </CommandList>
+          </CommandGroup>
+          <CreateCategoryDialog type={type} successCallback={successCallback} />
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default CategoryPicker;
