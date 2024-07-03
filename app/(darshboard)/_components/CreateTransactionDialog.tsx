221084455d82a6cb1feaef0e7ccdaccdba20e01b
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-07-03 17:05:20 -0300

    feat: add create transaction final dialog

--- app/(darshboard)/_components/CreateTransactionDialog.tsx
+++ app/(darshboard)/_components/CreateTransactionDialog.tsx
@@ -2,8 +2,10 @@
 
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -14,7 +16,7 @@ import {
   CreateTransactionSchema,
   CreateTransactionSchemaType,
 } from "@/schema/transaction";
-import { ReactNode, useCallback } from "react";
+import { ReactNode, useCallback, useState } from "react";
 import React from "react";
 import { Form, useForm, FormProvider } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -24,10 +26,23 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import CategoryPicker from "./CategoryPicker";
 import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import { CalendarIcon, Loader2 } from "lucide-react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { CreateTransaction } from "../_actions/transactions";
+import { toast } from "sonner";
+import { DateToUTCDate } from "@/lib/helpers";
 
 interface Props {
   trigger: ReactNode;
@@ -35,6 +50,8 @@ interface Props {
 }
 
 const CreateTransactionDialog = ({ trigger, type }: Props) => {
+  const [open, setOpen] = useState(false);
+
   const form = useForm<CreateTransactionSchemaType>({
     resolver: zodResolver(CreateTransactionSchema), //resolver:função para resolver a validação dos dados do formulário.
     defaultValues: {
@@ -43,12 +60,54 @@ const CreateTransactionDialog = ({ trigger, type }: Props) => {
     },
   });
 
-  const handleCategoryChange = useCallback((value: string) => {
-    form.setValue("category", value);
-  }, [form]);
+  const handleCategoryChange = useCallback(
+    (value: string) => {
+      form.setValue("category", value);
+    },
+    [form]
+  );
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: CreateTransaction,
+    onSuccess: () => {
+      toast.success("Transação criada com sucesso 🎉", {
+        id: "create-transaction",
+      });
+
+      form.reset({
+        type,
+        description: "",
+        amount: 0,
+        date: new Date(),
+        category: undefined,
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ["overview"],
+      });
+
+      setOpen((prev) => !prev);
+    },
+  });
+
+  const onSubmit = useCallback(
+    (values: CreateTransactionSchemaType) => {
+      toast.loading("Criando transação...", {
+        id: "create-transaction",
+      });
+
+      mutate({
+        ...values,
+        date: DateToUTCDate(values.date),
+      });
+    },
+    [mutate]
+  );
 
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
       <DialogContent>
         <DialogHeader>
@@ -68,87 +127,124 @@ const CreateTransactionDialog = ({ trigger, type }: Props) => {
           </DialogDescription>
         </DialogHeader>
         <FormProvider {...form}>
-          <form className="space-y-4">
-            <FormField
-              control={form.control} //controle pela const form
-              name="description" //nome no esquema de validação
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Descrição</FormLabel>
-                  <FormControl>
-                    <Input defaultValue={""} {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Descrição da transação (opcional)
-                  </FormDescription>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control} //controle pela const form
-              name="amount" //nome no esquema de validação
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Valor</FormLabel>
-                  <FormControl>
-                    <Input defaultValue={0} type="number" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Valor da transação (obrigatório)
-                  </FormDescription>
-                </FormItem>
-              )}
-            />
-            <div className="flex items-center">
+          <Form>
+            <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
               <FormField
                 control={form.control} //controle pela const form
-                name="category" //nome no esquema de validação
+                name="description" //nome no esquema de validação
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="mr-3">Categoria</FormLabel>
+                    <FormLabel>Descrição</FormLabel>
                     <FormControl>
-                      <CategoryPicker
-                        type={type}
-                        onChange={handleCategoryChange}
-                      />
+                      <Input defaultValue={""} {...field} />
                     </FormControl>
                     <FormDescription>
-                      Selecione a categoria da transação
+                      Descrição da transação (opcional)
                     </FormDescription>
                   </FormItem>
                 )}
               />
-            </div>
-            <FormField
-              control={form.control} //controle pela const form
-              name="description" //nome no esquema de validação
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Descrição</FormLabel>
-                  <FormControl>
-                    <Input defaultValue={""} {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Descrição da transação (opcional)
-                  </FormDescription>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control} //controle pela const form
-              name="date" //nome no esquema de validação
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Data</FormLabel>
-                  <FormControl>
-                    <Calendar {...field} />
-                  </FormControl>
-                  <FormDescription>Data da transação</FormDescription>
-                </FormItem>
-              )}
-            />
-          </form>
+              <FormField
+                control={form.control} //controle pela const form
+                name="amount" //nome no esquema de validação
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Valor</FormLabel>
+                    <FormControl>
+                      <Input defaultValue={0} type="number" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Valor da transação (obrigatório)
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between gap-4">
+                <FormField
+                  control={form.control} //controle pela const form
+                  name="category" //nome no esquema de validação
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="mr-3">Categoria</FormLabel>
+                      <FormControl>
+                        <CategoryPicker
+                          type={type}
+                          onChange={handleCategoryChange}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        Selecione a categoria da transação
+                      </FormDescription>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control} //controle pela const form
+                  name="date" //nome no esquema de validação
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Data da transação</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-[200px] pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Escolha a data</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            initialFocus
+                          ></Calendar>
+                        </PopoverContent>
+                      </Popover>
+                      <FormDescription>
+                        Selecione uma data para essa transação
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </form>
+          </Form>
         </FormProvider>
+        <DialogFooter className="flex">
+          <DialogClose asChild>
+            <Button
+              type="button"
+              variant={"secondary"}
+              onClick={() => {
+                form.reset();
+              }}
+            >
+              Cancelar
+            </Button>
+          </DialogClose>
+          <Button
+            onClick={form.handleSubmit(onSubmit)}
+            disabled={isPending}
+            type="submit"
+          >
+            {!isPending && "Criar"}
+            {isPending && <Loader2 className="animate-spin" />}
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
