c9219c34ecadf5914ffad456fdfc7ffefc6955ea
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-07-08 02:23:19 -0300

    feat: add route to get balance statistics

---
+++ app/api/stats/balance/route.ts
@@ -0,0 +1,54 @@
+import prisma from "@/lib/prisma";
+import { OverviewQuerySchema } from "@/schema/overview";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export async function GET(request: Request) {
+    const user = await currentUser();
+    if (!user) {
+        redirect("/sign-in");
+    }
+
+    const { searchParams } = new URL(request.url);
+    const from = searchParams.get("from");
+    const to = searchParams.get("to");
+
+    const queryParams = OverviewQuerySchema.safeParse({ from, to });
+
+    if (!queryParams.success) {
+        return Response.json(queryParams.error.message, {
+            status: 400
+        })
+    }
+
+    const stats = await getBalanceStats(
+        user.id,
+        queryParams.data.from,
+        queryParams.data.to
+    );
+
+    return Response.json(stats);
+}
+
+export type GetBalanceStatsResponseType = Awaited<ReturnType<typeof getBalanceStats>>;
+
+const getBalanceStats = async (userId: string, from: Date, to: Date) => {
+    const totals = await prisma.transaction.groupBy({
+        by: ["type"],
+        where: {
+            userId,
+            date: {
+                gte: from,
+                lte: to
+            },
+        },
+        _sum: {
+            amount: true
+        },
+    })
+
+    return {
+        despesa: totals.find(t => t.type === "despesa")?._sum.amount || 0,
+        renda: totals.find(t => t.type === "renda")?._sum.amount || 0
+    }
+}
